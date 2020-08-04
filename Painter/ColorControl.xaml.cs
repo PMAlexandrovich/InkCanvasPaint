8fb56a505caf411feb1eb56051ef8f3bb0f924f2
Max <maxima-2000@mail.ru>
2020-08-04 22:47:13 +0300

    Add project files.

---
+++ Painter/ColorControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Painter
+{
+    /// <summary>
+    /// Логика взаимодействия для ColorControl.xaml
+    /// </summary>
+    public partial class ColorControl : UserControl
+    {
+        public ColorControl()
+        {
+            
+            InitializeComponent();
+        }
+
+        byte r=255, g=255, b=255, a=255;
+
+        public byte A
+        {
+            get { return a; }
+            set
+            {
+                a = (byte)value;
+                Color = Color.FromArgb(a, r, g, b);
+            }
+        }
+        public byte R
+        {
+            get { return r; }
+            set
+            {
+                r = (byte)value;
+                Color = Color.FromArgb(a, r, g, b);
+            }
+        }
+        public byte G
+        {
+            get { return g; }
+            set
+            {
+                g = (byte)value;
+                Color = Color.FromArgb(a, r, g, b);
+            }
+        }
+        public byte B
+        {
+            get { return b; }
+            set
+            {
+                b = (byte)value;
+                Color = Color.FromArgb(a, r, g, b);
+            }
+        }
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Color), typeof(ColorControl));
+
+
+    }
+}
---
+++ Painter/View Model.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Ink;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Media;
+using GalaSoft;
+using CommonServiceLocator;
+
+
+namespace Painter
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+    class ViewModel:ViewModelBase
+    {
+        public ViewModel()
+        {
+            PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == "CurrentColor")
+                {
+                    DrawingAttributes.Color = ((ViewModel)o).CurrentColor;
+                    OnPropertyChanged("DrawingAttributes");
+                }
+            };
+        }
+
+        InkCanvas InkCanvas;
+
+        Color currentColor = new Color();
+
+        DrawingAttributes drawingAttributes = new DrawingAttributes();
+        InkCanvasEditingMode editingMode = InkCanvasEditingMode.Ink;
+        StrokeCollection strokes = new StrokeCollection();
+
+        Point firstPos;
+
+        Point lastPos;
+
+
+        public StrokeCollection Strokes
+        {
+            get
+            {
+                return strokes;
+            }
+            set
+            {
+                strokes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DrawingAttributes DrawingAttributes
+        {
+            get
+            {
+                return drawingAttributes;
+            }
+            set
+            {
+                drawingAttributes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Color CurrentColor
+        {
+            get
+            {
+                return currentColor;
+            }
+            set
+            {
+                currentColor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public InkCanvasEditingMode EditingMode
+        {
+            get
+            {
+                return editingMode;
+            }
+            set
+            {
+                editingMode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SomeC
+        {
+            get
+            {
+            
+                return new RelayCommand<Object>(new Action<Object>(ChangeMode));
+            }
+        }
+        public void ChangeMode(Object o)
+        {
+            ComboBoxItem cb = (ComboBoxItem)o;
+            switch (cb.Tag.ToString())
+            {
+                case "Selection":
+                    EditingMode = InkCanvasEditingMode.Select;
+                    break;
+                case "Ink":
+                    EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "EraseByPoint":
+                    EditingMode = InkCanvasEditingMode.EraseByPoint;
+                    break;
+                case "EraseByStroke":
+                    EditingMode = InkCanvasEditingMode.EraseByStroke;
+                    break;
+            }
+        }
+    }
+}
